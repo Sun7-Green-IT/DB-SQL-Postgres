a9cc6cf3f886a54b6ead59315d30acb222fa0809
Sylvain <sylvain.denyse@gmail.com>
2018-10-31 14:54:24 +0100

    update init.sql

---
+++ init.sql
@@ -0,0 +1,21 @@
+CREATE EXTENSION IF NOT EXISTS "uuid-ossp";
+
+CREATE TABLE users (
+    UsersID uuid DEFAULT uuid_generate_v4 (),
+    Firstname varchar(25)  NOT NULL,
+    Lastname varchar(25)   NOT NULL,
+    Birthday date   NOT NULL,
+    email varchar(50)  NOT NULL,
+    RolesID int NOT NULL,
+    PRIMARY KEY (UsersID),
+    FOREIGN KEY (RolesID) REFERENCES roles(RolesID)
+);
+
+CREATE TABLE roles (
+    RolesID int NOT NULL,
+    Role varchar(25)  NOT NULL,
+    PRIMARY KEY (RolesID)
+);
+
+CREATE INDEX "idx_Users_Firstname"
+ON users ("email");
